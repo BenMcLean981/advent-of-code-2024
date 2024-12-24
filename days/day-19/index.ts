a6420b31e698db55bdea1e8b1f45515d40d2efc2
Ben McLean <35312653+BenMcLean981@users.noreply.github.com>
2024-12-24 13:29:26 -0500

    Added empty days.

---
+++ days/day-19/index.ts
@@ -0,0 +1,27 @@
+import * as path from "jsr:@std/path";
+
+import { readLines } from "../../utils.ts";
+
+export function solveDay19(filename: string) {
+  const fullPath = path.join(import.meta.dirname ?? "", filename);
+
+  const lines = readLines(fullPath);
+  const input = parseInput(lines);
+
+  console.log(`The solution to part 1 is: ${solvePart1(input)}`);
+  console.log(`The solution to part 2 is: ${solvePart2(input)}`);
+}
+
+type Input = {};
+
+function parseInput(lines: ReadonlyArray<string>): Input {
+  return {};
+}
+
+function solvePart1(input: Input): number {
+  return 0;
+}
+
+function solvePart2(input: Input): number {
+  return 0;
+}
--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
-import { solveDay10 } from "./days/day-10/index.ts";
 import {
   solveDay1,
+  solveDay11,
+  solveDay12,
+  solveDay13,
+  solveDay14,
+  solveDay15,
+  solveDay16,
+  solveDay17,
+  solveDay18,
+  solveDay19,
   solveDay2,
+  solveDay20,
+  solveDay21,
+  solveDay22,
+  solveDay23,
+  solveDay24,
+  solveDay25,
   solveDay3,
   solveDay4,
   solveDay5,
   solveDay6,
+  solveDay10,
   solveDay7,
   solveDay8,
   solveDay9,
@@ -56,6 +71,51 @@ if (import.meta.main) {
       break;
     case "10":
       solveDay10(file);
+      break;
+    case "11":
+      solveDay11(file);
+      break;
+    case "12":
+      solveDay12(file);
+      break;
+    case "13":
+      solveDay13(file);
+      break;
+    case "14":
+      solveDay14(file);
+      break;
+    case "15":
+      solveDay15(file);
+      break;
+    case "16":
+      solveDay16(file);
+      break;
+    case "17":
+      solveDay17(file);
+      break;
+    case "18":
+      solveDay18(file);
+      break;
+    case "19":
+      solveDay19(file);
+      break;
+    case "20":
+      solveDay20(file);
+      break;
+    case "21":
+      solveDay21(file);
+      break;
+    case "22":
+      solveDay22(file);
+      break;
+    case "23":
+      solveDay23(file);
+      break;
+    case "24":
+      solveDay24(file);
+      break;
+    case "25":
+      solveDay25(file);
       break;
     default:
       throw new Error(`Unsupported day "${day}".`);
